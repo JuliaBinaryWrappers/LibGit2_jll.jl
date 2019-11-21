e9276b5ae76f6de92b965150e3e171884a8181c6
jlbuild <juliabuildbot@gmail.com>
2019-11-21 06:11:54 +0000

    LibGit2_jll build 0.28.2+0

---
+++ src/LibGit2_jll.jl
@@ -0,0 +1,31 @@
+module LibGit2_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("e37daf67-58a4-590a-8e99-b0245dd2ffc5"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "LibGit2", artifacts_toml) for e in artifacts["LibGit2"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load LibGit2; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module LibGit2_jll

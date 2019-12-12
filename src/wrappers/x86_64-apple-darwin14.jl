29cc93e04243d6994c1c98d615364c23e36eb7dd
jlbuild <juliabuildbot@gmail.com>
2019-12-12 19:12:33 +0000

    LibGit2_jll build 0.28.2+1

--- src/wrappers/x86_64-apple-darwin14.jl
+++ src/wrappers/x86_64-apple-darwin14.jl
@@ -27,14 +27,18 @@ const libgit2 = "@rpath/libgit2.28.dylib"
 Open all libraries
 """
 function __init__()
-    global prefix = abspath(joinpath(@__DIR__, ".."))
+    global artifact_dir = abspath(artifact"LibGit2")
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    append!.(Ref(PATH_list), (MbedTLS_jll.PATH_list, LibSSH2_jll.PATH_list, LibCURL_jll.PATH_list,))
-    append!.(Ref(LIBPATH_list), (MbedTLS_jll.LIBPATH_list, LibSSH2_jll.LIBPATH_list, LibCURL_jll.LIBPATH_list,))
+    # We first need to add to LIBPATH_list the libraries provided by Julia
+    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
+    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
+    # then append them to our own.
+    foreach(p -> append!(PATH_list, p), (MbedTLS_jll.PATH_list, LibSSH2_jll.PATH_list, LibCURL_jll.PATH_list,))
+    foreach(p -> append!(LIBPATH_list, p), (MbedTLS_jll.LIBPATH_list, LibSSH2_jll.LIBPATH_list, LibCURL_jll.LIBPATH_list,))
 
-    global libgit2_path = abspath(joinpath(artifact"LibGit2", libgit2_splitpath...))
+    global libgit2_path = normpath(joinpath(artifact_dir, libgit2_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
     # of `ccall` with its `SONAME` will find this path immediately.

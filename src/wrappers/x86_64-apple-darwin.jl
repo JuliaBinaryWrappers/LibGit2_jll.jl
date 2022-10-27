9df2457bef279a0da6a2b06db0588f129890f804
jlbuild <juliabuildbot@gmail.com>
2022-10-27 19:47:17 +0000

    LibGit2_jll build 1.5.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -4,12 +4,12 @@ export libgit2
 using MbedTLS_jll
 using LibSSH2_jll
 JLLWrappers.@generate_wrapper_header("LibGit2")
-JLLWrappers.@declare_library_product(libgit2, "@rpath/libgit2.1.4.dylib")
+JLLWrappers.@declare_library_product(libgit2, "@rpath/libgit2.1.5.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(MbedTLS_jll, LibSSH2_jll)
     JLLWrappers.@init_library_product(
         libgit2,
-        "lib/libgit2.1.4.4.dylib",
+        "lib/libgit2.1.5.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 

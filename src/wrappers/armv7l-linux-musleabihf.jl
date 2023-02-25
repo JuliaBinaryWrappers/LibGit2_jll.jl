631ce3488b0288208b619eeeb74e62424ab66e3a
jlbuild <juliabuildbot@gmail.com>
2023-02-25 20:33:36 +0000

    LibGit2_jll build 1.6.1+0

--- src/wrappers/armv7l-linux-musleabihf.jl
+++ src/wrappers/armv7l-linux-musleabihf.jl
@@ -4,7 +4,7 @@ export libgit2
 using MbedTLS_jll
 using LibSSH2_jll
 JLLWrappers.@generate_wrapper_header("LibGit2")
-JLLWrappers.@declare_library_product(libgit2, "libgit2.so.1.5")
+JLLWrappers.@declare_library_product(libgit2, "libgit2.so.1.6")
 function __init__()
     JLLWrappers.@generate_init_header(MbedTLS_jll, LibSSH2_jll)
     JLLWrappers.@init_library_product(

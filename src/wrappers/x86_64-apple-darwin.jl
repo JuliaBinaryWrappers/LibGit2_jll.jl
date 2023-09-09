ecc3a091a36e112501c831138eb12e31ff1ab92b
jlbuild <juliabuildbot@gmail.com>
2023-09-09 00:43:08 +0000

    LibGit2_jll build 1.7.1+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -4,12 +4,12 @@ export libgit2
 using MbedTLS_jll
 using LibSSH2_jll
 JLLWrappers.@generate_wrapper_header("LibGit2")
-JLLWrappers.@declare_library_product(libgit2, "@rpath/libgit2.1.6.dylib")
+JLLWrappers.@declare_library_product(libgit2, "@rpath/libgit2.1.7.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(MbedTLS_jll, LibSSH2_jll)
     JLLWrappers.@init_library_product(
         libgit2,
-        "lib/libgit2.1.6.4.dylib",
+        "lib/libgit2.1.7.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 

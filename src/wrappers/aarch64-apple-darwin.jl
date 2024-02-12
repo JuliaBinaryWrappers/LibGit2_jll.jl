627fd74d80ad6eb0f3c213633c725aa3b4ce5f21
jlbuild <juliabuildbot@gmail.com>
2024-02-12 15:59:19 +0000

    LibGit2_jll build 1.7.2+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -9,7 +9,7 @@ function __init__()
     JLLWrappers.@generate_init_header(MbedTLS_jll, LibSSH2_jll)
     JLLWrappers.@init_library_product(
         libgit2,
-        "lib/libgit2.1.7.1.dylib",
+        "lib/libgit2.1.7.2.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
